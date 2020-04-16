6f5c2a208c6abca650b9359a8a5ee0ddb16484ce
yoogle96 <dmltjs851@gmail.com>
2020-04-16 18:58:39 +0900

    Add UnitManager
    - 유닛 관리 클래스 생성

---
+++ src/yoocraft/manager/UnitManager.java
@@ -0,0 +1,14 @@
+package yoocraft.manager;
+
+public class UnitManager {
+
+    private static UnitManager instance = new UnitManager();
+
+    public static UnitManager Instance() {
+        return instance;
+    }
+
+    public void update() {
+
+    }
+}
